72c3027e44cc6fc8edbeeaba0fc71359fca6e7f6
seferagicr <raisa.seferagic@transcore.com>
2021-01-23 14:58:53 +0100

    deploy update

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+app.use(express.static('./dist/angular-app'));
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'dist/angular-app/'}
+  );
+});
+app.listen(process.env.PORT || 8090);
